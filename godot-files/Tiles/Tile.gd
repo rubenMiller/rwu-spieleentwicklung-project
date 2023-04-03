fe7497d02bfb756a9492cf26d8340783f20f414d
Ruben Miller <rm-202356@hs-weingarten.de>
2023-04-03 12:24:48 +0200

    Added colors on click to the tiles.

--- godot-files/Tiles/Tile.gd
+++ godot-files/Tiles/Tile.gd
@@ -2,13 +2,19 @@ extends KinematicBody
 
 signal tile_selected(vector)
 
+
+var selectedMaterial: Material = load("res://Art/materials/tile_selected.tres")
+var unselectedMaterial: Material = load("res://Art/materials/tile_unselected.tres")
+
 func _ready():
-	pass # Replace with function body.
+	$MeshInstance.material_override = unselectedMaterial
 
 
 func _on_Area_input_event(camera, event, position, normal, shape_idx):
-	var material = self.get_node("MeshInstance").get_surface_material(0)
 	if event.is_action_pressed("mouse_left"):
 		print("I am node at: ", global_transform.origin)
+		$MeshInstance.material_override = selectedMaterial
 		emit_signal("tile_selected", self)
 		print("signal emitted")
+	if event.is_action_pressed("mouse_right"):
+		$MeshInstance.material_override = unselectedMaterial
