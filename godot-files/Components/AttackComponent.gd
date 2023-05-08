545c970d6655be19de7d02c92b9a158e2bcacbfd
Petruz00732 <petrzubarev@gmx.de>
2023-05-08 11:48:03 +0200

    scuffed_reloadBar

---
+++ godot-files/Components/AttackComponent.gd
@@ -0,0 +1,8 @@
+extends Node
+
+export var fire_rate = 1
+export var damage = 1
+
+func _process(delta: float) -> void:
+	
+
--- godot-files/enemies/Enemy-Tower.gd
+++ godot-files/enemies/Enemy-Tower.gd
@@ -3,6 +3,8 @@ extends Spatial
 
 var recharge_time = 2 # is 2 seconds
 var elapsed_time = 0
+#onready var timer = get_node(Timer);
+onready var health_bar = $Sprite3D
 var ready_to_shoot: bool = false
 export (NodePath) var troop1Path
 onready var troop1Node = get_node(troop1Path)
@@ -20,6 +22,7 @@ func _ready():
 func _process(delta):
 	if ready_to_shoot == false:
 		elapsed_time += delta
+		health_bar.scale.x = (2 - elapsed_time) /2
 		if elapsed_time >= recharge_time:
 			print("Relad successfull!")
 			ready_to_shoot = true
