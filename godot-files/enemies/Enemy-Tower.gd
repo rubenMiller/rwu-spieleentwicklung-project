d9a527f9f16c32cd22d2c4bdd2b743ee0ed3ad97
Petruz00732 <petrzubarev@gmx.de>
2023-05-15 10:35:26 +0200

    refactored tower shooting

--- godot-files/enemies/Enemy-Tower.gd
+++ godot-files/enemies/Enemy-Tower.gd
@@ -1,47 +1,30 @@
 extends Spatial
 
-
-var recharge_time = 2 # is 2 seconds
-var elapsed_time = 0
-#onready var timer = get_node(Timer);
-onready var health_bar = $Sprite3D
-var ready_to_shoot: bool = false
+onready var reload_timer = $Reload_timer
+onready var health_bar = $Health_bar
 var target_list = []
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ready_to_shoot == false:
-		elapsed_time += delta
-		health_bar.scale.x = (2 - elapsed_time) /2
-		if elapsed_time >= recharge_time:
-			print("Relad successfull!")
-			ready_to_shoot = true
+	health_bar.scale.x = reload_timer.time_left / reload_timer.wait_time
+	if not reload_timer.is_stopped():
+		return
 	
-	if target_list.size() > 0 && ready_to_shoot:
-		# print("The node ", target_list[0], " will be shot at")
+	if target_list.size():
 		shoot_first_target()
-		elapsed_time = 0
-		ready_to_shoot = false
+		reload_timer.start()
 		
 func shoot_first_target():
 	print("Going to shoot: ", target_list[0])
-	# get_node(target_list[0]).is_queued_for_deletion()
 	print("target_list", target_list, "target_list")
 	target_list[0].get_child(4).reduceHealth(1)
 	print("Reduced Health by on for: ", target_list[0])
-	# target_list.erase[target_list[0]]
-
 
 func _on_radius_body_entered(body):
 	print("Body entered: ", body)
 	target_list.append(body)
-	
 	
 func _on_radius_body_exited(body):
 	print("Body entered: ", body)
