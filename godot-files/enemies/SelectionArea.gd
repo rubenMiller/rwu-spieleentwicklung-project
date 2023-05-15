2458ab99806af94b0bd58f4139c3d629303e98a3
Petruz00732 <petrzubarev@gmx.de>
2023-05-15 11:38:50 +0200

    extractred attack component

--- godot-files/enemies/SelectionArea.gd
+++ godot-files/enemies/SelectionArea.gd
@@ -1,8 +1,8 @@
 extends Area
 
 export var selection_action = "mouse_left"
-export (NodePath) var radiusPath
-onready var radiusNode = get_node(radiusPath)
+export (NodePath) var attack_component_path
+onready var attack_component = get_node(attack_component_path)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,5 +12,6 @@ func _ready():
 
 func _input_event(_camera: Object, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event.is_action_pressed(selection_action):
-		radiusNode.get_child(0).visible = not radiusNode.get_child(0).visible
+		print("pressed")
+		attack_component.change_radius_visibility()
 
