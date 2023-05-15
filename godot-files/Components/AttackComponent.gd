bdc0d7e4da869253c4d4650dcdcf10e7ec152b17
Petruz00732 <petrzubarev@gmx.de>
2023-05-15 14:03:52 +0200

    attack component with troops

--- godot-files/Components/AttackComponent.gd
+++ godot-files/Components/AttackComponent.gd
@@ -1,6 +1,6 @@
 extends Node
 
-export var target_group_name := " "
+export var target_group_name := ""
 onready var reload_timer = $Reload_timer
 onready var health_bar = $Health_bar
 onready var radius = $radius
@@ -23,16 +23,20 @@ func _process(delta):
 func shoot_first_target():
 	#print("Going to shoot: ", target_list[0])
 	#print("target_list", target_list, "target_list")
-	target_list[0].get_child(4).reduceHealth(1)
+	target_list[0].get_child(0).reduceHealth(1)
 	#print("Reduced Health by on for: ", target_list[0])
 
 func _on_radius_body_entered(body):
 	#print("Body entered: ", body)
 	#print("something entered")
 	#print(body.get_groups())
-	print(get_tree().get_nodes_in_group("Troops"))
+	#print(get_tree().get_nodes_in_group("Troops"))
+	#if(target_group_name == "Towers"):
+	print("Want to attack: ", target_group_name, " body: ", body)
+		
 	if body.is_in_group(target_group_name):
 		#print("entered" + target_group_name)
+		print("Adding to list from group: ", target_group_name)
 		target_list.append(body)
 	
 func _on_radius_body_exited(body):
