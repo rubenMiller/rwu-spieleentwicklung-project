531c0ef5fa2fae0f58282367ae3e48c84c73b317
Petruz00732 <petrzubarev@gmx.de>
2023-06-05 11:53:06 +0200

    death on mortar

--- godot-files/Entities/impact_area.gd
+++ godot-files/Entities/impact_area.gd
@@ -1,6 +1,19 @@
 extends Area
 
-
+var target_list := []
 
 func _on_Timer_timeout() -> void:
 	queue_free()
+	if target_list.size() > 0:
+		for e in target_list:
+			e.queue_free()
+			#target_list.erase(e)
+
+
+func _on_Impact_area_body_entered(body: Node) -> void:
+	print(body)
+	target_list.append(body)
+
+
+func _on_Impact_area_body_exited(body: Node) -> void:
+	target_list.erase(body)
