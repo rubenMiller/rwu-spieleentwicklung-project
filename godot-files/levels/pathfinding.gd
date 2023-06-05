a07823db5157687b0fe3dfa969eff5df7cbec96c
Ruben Miller <rm-202356@hs-weingarten.de>
2023-06-05 11:07:52 +0200

    Added a Lost-Screen when all Troops are Dead.

--- godot-files/levels/pathfinding.gd
+++ godot-files/levels/pathfinding.gd
@@ -1,6 +1,10 @@
 extends Spatial
 
-
+func _process(delta):
+	if $Troops.get_child_count() <= 0:
+		$UserInterface/Retry/Label_won.visible = false
+		$UserInterface/Retry/Label_lost.visible = true
+		$UserInterface.show()
 
 func _ready():
 	SignalBus.connect("won", self, "_on_won")
@@ -12,5 +16,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		get_tree().reload_current_scene()
 
 func _on_won():
+	$UserInterface/Retry/Label_won.visible = true
+	$UserInterface/Retry/Label_lost.visible = false
 	$UserInterface.show()
 	
