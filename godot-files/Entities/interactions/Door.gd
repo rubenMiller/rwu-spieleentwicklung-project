fc05315692c25b8f4a3ba9e31918cf4b26a97d87
Petruz00732 <petrzubarev@gmx.de>
2023-06-26 14:06:51 +0200

    animated door

--- godot-files/Entities/interactions/Door.gd
+++ godot-files/Entities/interactions/Door.gd
@@ -4,6 +4,8 @@ onready var mesh_instance: MeshInstance = $MeshInstance
 export (Resource) var folder
 var enabled = false
 var nav_mesh
+onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var collision_shape: CollisionShape = $CollisionShape
 
 func _ready() -> void:
 	nav_mesh = get_tree().get_nodes_in_group("navigation_mesh_instance")[0]
@@ -14,10 +16,17 @@ func on_interaction(value):
 	enabled = value
 	
 	if enabled:
-		translation.y = -2
-		nav_mesh.update_mesh()
+		animation_player.play("Door_1_open")
+		#collision_shape.translation.y = -2
+		#nav_mesh.update_mesh()
 		print("door opened")
 	if not enabled:
-		translation.y = 3
-		nav_mesh.update_mesh()
+		animation_player.play_backwards("Door_1_open")
+		#collision_shape.translation.y = 3
+		#nav_mesh.update_mesh()
 		print("door closed")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	print("animation finished")
+	nav_mesh.update_mesh()
