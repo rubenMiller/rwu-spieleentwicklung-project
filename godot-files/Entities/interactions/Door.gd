ac29426cc956b0a0ae7be7004e748d6a86f951e2
Petruz00732 <petrzubarev@gmx.de>
2023-06-26 11:35:43 +0200

    towers linear rotation implemented
    thx chatgpt

--- godot-files/Entities/interactions/Door.gd
+++ godot-files/Entities/interactions/Door.gd
@@ -1,12 +1,13 @@
 extends StaticBody
 
-
+onready var mesh_instance: MeshInstance = $MeshInstance
+export (Resource) var folder
 var enabled = false
 var nav_mesh
 
-
 func _ready() -> void:
 	nav_mesh = get_tree().get_nodes_in_group("navigation_mesh_instance")[0]
+	#mesh_instance.material/0 = 
 	#print(nav_mesh)
 
 func on_interaction(value):
--- godot-files/Entities/troop_tower.gd
+++ godot-files/Entities/troop_tower.gd
@@ -16,13 +16,36 @@ func _ready():
 	
 func _process(delta: float) -> void:
 	if attack_component.current_target != null:
-		rotate_tower()
+		rotate_tower(delta)
 		
-func rotate_tower():
+func rotate_tower(delta):
 	var target = attack_component.current_target
 	var v = target.global_translation - global_translation
-	body.global_rotation.y = lerp_angle(body.global_rotation.y, atan2(v.x,v.z), rotate_speed)
-	view_component.global_rotation.y = lerp_angle(view_component.global_rotation.y, atan2(v.x,v.z), rotate_speed)
+	var target_rotation = atan2(v.x,v.z) 
+
+	var relative_rotation = target_rotation - body.global_rotation.y
+	
+	# ChatGPT <3
+	if relative_rotation > PI:
+		relative_rotation -= 2 * PI
+	if relative_rotation < -PI:
+		relative_rotation += 2 * PI
+	print("y: ", body.global_rotation.y, " ,target rotation: ", target_rotation, " , realtive rotation: ", relative_rotation)
+	
+	if abs(target_rotation - body.global_rotation.y) < 0.05:
+		return
+	#elif body.global_rotation.y - target_rotation > 0.1:
+	
+	if relative_rotation > 0:
+		body.global_rotation.y += rotate_speed
+		view_component.global_rotation.y += rotate_speed
+	else:
+		body.global_rotation.y -= rotate_speed
+		view_component.global_rotation.y -= rotate_speed
+
+	#body.global_rotation.y = lerp_angle(body.global_rotation.y, atan2(v.x,v.z), rotate_speed)
+	#view_component.global_rotation.y = lerp_angle(view_component.global_rotation.y, atan2(v.x,v.z), rotate_speed)
+
 	
 func _on_SelectionArea_selection_toggled(selection):
 	isSelected = selection
